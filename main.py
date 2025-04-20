718b109d8df21eb8fec5c360c961cf9a940486df
pstt <xyamix1337@gmail.com>
2025-04-20 19:35:09 +0300

    renamed method voice to sai

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ class Animal(ABC):
         self.name = name
     
     @abstractmethod
-    def voice():
+    def sai():
         pass
 
 class Dog(Animal):
     def __init__(self):
         super().__init__("Dog")
 
-    def voice(self):
+    def sai(self):
         print("Woof!")
 
 if __name__ == "__main__":
     dog = Dog()
-    dog.voice()
+    dog.sai()
